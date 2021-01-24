e143201cfcf73d31837cdb8c91a6d132bb32615c
Omar Baldi <omarbaldidk@gmail.com>
2021-01-24 13:03:35 +0100

    Implemented use of react router dom history to go back to the previous URL when click outside the sidebar

--- src/components/PokemonInfo.js
+++ src/components/PokemonInfo.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useLocation } from "react-router-dom";
-
+import { Drawer } from '@material-ui/core'
 
 export default function About(props) {
+
     const data = useLocation();
     const { info: pokemonInfo } = data.state;
+    const [showSidebar, setShowSideBar] = useState(false);
+
+    useEffect(() => {
+        changeSideBarValue(true);
+    }, [pokemonInfo]);
+
+    const changeSideBarValue = (open) => {
+        setShowSideBar(open);
+
+        if (!open) {
+            return props.history.goBack();
+        }
+    };
 
     return (
-        <div>
+        <Drawer open={showSidebar} onClose={() => changeSideBarValue(false)}>
             <h4>Show pokemon info for {pokemonInfo.name} </h4>
             <ul>
                 {
@@ -20,6 +34,6 @@ export default function About(props) {
                     })
                 }
             </ul>
-        </div>
+        </Drawer>
     )
 }
